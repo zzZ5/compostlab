f11bab52c5115f32e36c13d9280d2b20d1037001
zzZ5 <baoju_liu@foxmail.com>
2021-04-23 15:17:28 +0800

    添加data应用

---
+++ data/models.py
@@ -0,0 +1,70 @@
+from django.contrib.auth.models import User
+from django.db import models
+from equipment.models import Equipment
+
+
+class Sensor(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    name_brief = models.CharField(max_length=64, null=True)
+    key = models.CharField(max_length=16, unique=True, null=True)
+
+    TEMPERQTURE = 'T'
+    HUMIDITY = 'H'
+    TYPE_CHOICE = (
+        (TEMPERQTURE, "Temperature sensor"),
+        (HUMIDITY, "Humidity sensor")
+    )
+
+    type = models.CharField(
+        max_length=32, choices=TYPE_CHOICE, default=TEMPERQTURE)
+
+    descript = models.CharField(max_length=256, null=True)
+
+    equipment = models.ForeignKey(
+        Equipment, null=True, on_delete=models.SET_NULL)
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = "sensor"
+        ordering = ["-created_time"]
+        verbose_name = "Sensor"
+        verbose_name_plural = "Sensors"
+
+
+class Sensor_Record(models.Model):
+
+    modifier = models.ForeignKey(
+        User, null=True, on_delete=models.SET_NULL)
+    sensor = models.ForeignKey(Sensor, null=True, on_delete=models.CASCADE)
+    record = models.CharField(max_length=256, blank=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.record
+
+    class Meta:
+        db_table = "sensor_record"
+        ordering = ["-created_time"]
+        verbose_name = "SensorRecord"
+        verbose_name_plural = "SensorRecords"
+
+
+class data(models.Model):
+    sensor = models.ForeignKey(
+        Sensor, null=True, on_delete=models.CASCADE)
+    value = models.FloatField()
+    unit = models.CharField(max_length=32, null=True)
+    measured_time = models.DateTimeField(null=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(round(self.value, 2))
+
+    class Meta:
+        db_table = "data"
+        ordering = ["-created_time"]
+        verbose_name = "Data"
+        verbose_name_plural = "Datas"
---
+++ data/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from account.serializers import BriefUserSerializer
+from data.models import Sensor, Sensor_Record
+from equipment.serializers import EquipmentSerializer
+
+
+class SensorSerializer(serializers.ModelSerializer):
+    equipment = EquipmentSerializer(required=False)
+
+    class Meta:
+        model = Sensor
+        fields = ('id', 'name', 'name_brief', 'key',
+                  'type', 'descript', 'equipment', 'created_time')
+        depth = 1
+
+
+class SensorRecordSerializer(serializers.ModelSerializer):
+    modifier = BriefUserSerializer(required=False)
+    sensor = SensorSerializer(required=False)
+
+    class Meta:
+        model = Sensor_Record
+        fields = ('record', 'sensor', 'modifier', 'created_time')
+        depth = 1
