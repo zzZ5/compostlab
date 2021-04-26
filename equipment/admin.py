2bbbc8c1ee55e079f8580966f53d2ca18dcc3b4f
zzZ5 <baoju_liu@foxmail.com>
2021-04-26 17:19:58 +0800

    加入传感器

--- equipment/admin.py
+++ equipment/admin.py
@@ -1,8 +1,28 @@
 from django.contrib import admin
+from django.contrib.admin.widgets import FilteredSelectMultiple
+from django.forms import ModelMultipleChoiceField, ModelForm
 from equipment.models import Equipment, EquipmentRecordModify
+from data.models import Sensor
+
+
+class SensorAdminForm(ModelForm):
+    sensors = ModelMultipleChoiceField(
+        queryset=Sensor.objects.all(),
+        widget=FilteredSelectMultiple(verbose_name='sensors', is_stacked=False))
+
+    class Meta:
+        model = Sensor
+        fields = ['name', 'name_brief', 'type']
+
+    def __init__(self, *args, **kwargs):
+        super(SensorAdminForm, self).__init__(*args, **kwargs)
+        if self.instance:
+            # fill initial related values
+            self.fields['sensors'].initial = self.instance.sensors.all()
 
 
 class EquipmentAdmin(admin.ModelAdmin):
+    form = SensorAdminForm
     list_display = ('name', 'name_brief', 'pk', 'key',
                     'type', 'descript', 'sensors_display', 'begin_time',
                     'end_time', 'created_time', 'owner', 'users_display', 'is_inuse')
@@ -15,6 +35,19 @@ class EquipmentAdmin(admin.ModelAdmin):
         ('owner', {'fields': ['owner']}),
         ('created_time', {'fields': ['created_time']}),
     ]
+    filter_horizontal = ('users',)
+
+    def save_model(self, request, obj, form, change):
+        original_sensors = obj.sensors.all()
+        new_sensors = form.cleaned_data['sensors']
+        remove_qs = original_sensors.exclude(id__in=new_sensors.values('id'))
+        add_qs = new_sensors.exclude(id__in=original_sensors.values('id'))
+        for item in remove_qs:
+            obj.sensors.remove(item)
+        for item in add_qs:
+            obj.sensors.add(item)
+        obj.save()
+
     readonly_fields = ['created_time', 'key']
 
 
