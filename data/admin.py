718ab53a8b2d8a52d73728a6bfbef6883d0f7dbb
zzZ5 <baoju_liu@foxmail.com>
2021-04-26 17:19:21 +0800

    传感器管理

--- data/admin.py
+++ data/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from data.models import Sensor
 
-# Register your models here.
+
+class SensorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'name_brief', 'pk', 'key',
+                    'type', 'descript', 'equipment', 'created_time')
+    list_filter = ['equipment', 'created_time']
+    fieldsets = [
+        ('sensor name', {'fields': ['name', 'name_brief']}),
+        ('sensor information', {'fields': [
+         'type', 'descript', 'key', 'equipment']}),
+        ('created_time', {'fields': ['created_time']}),
+    ]
+    readonly_fields = ['created_time', 'key']
+
+
+admin.site.register(Sensor, SensorAdmin)
