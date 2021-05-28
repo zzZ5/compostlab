e66db8e0b13f36fdadb982668f7cbe68ce6ecebe
zzZ5 <baoju_liu@foxmail.com>
2021-05-28 17:26:17 +0800

    添加用户列表接口

--- account/views.py
+++ account/views.py
@@ -3,6 +3,8 @@ from compostlab.utils.pagination import RecordPagination
 
 from django.contrib.auth import authenticate, logout
 from django.contrib.auth.models import User
+import django_filters.rest_framework
+from rest_framework import filters
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
@@ -14,6 +16,13 @@ class UserViewSet(GenericViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (IsAuthenticated,)
+    pagination_class = RecordPagination
+    filter_backends = (django_filters.rest_framework.DjangoFilterBackend,
+                       filters.OrderingFilter, filters.SearchFilter,)
+    filter_fields = ('id', 'username', 'email', 'is_active',
+                     'is_staff', 'is_superuser', 'date_joined')
+    ordering_fields = ('id', 'name', 'date_joined')
+    search_fields = ('id', 'name', 'email')
 
     @ action(methods=['post'], detail=False, url_path='register', permission_classes=[IsAdminUser])
     def create_user(self, request, version, format=None):
@@ -59,6 +68,33 @@ class UserViewSet(GenericViewSet):
         serializer = self.get_serializer(instance=user)
         response_dict['message'] = 'Success'
         response_dict['data'] = serializer.data
+        return Response(data=response_dict, status=status.HTTP_200_OK)
+
+    @ action(methods=['get'], detail=False, url_path='list', permission_classes=[IsAuthenticated])
+    def get_list(self, request, version, format=None):
+        '''
+        Show all equipments through get.
+        Example:
+            GET 127.0.0.1:8000/api/1.0/equipment/list/?page=1&size=5
+        Return:
+            All equipments's information.
+        '''
+        response_dict = {'code': 200, 'message': 'ok', 'data': []}
+        queryset = self.get_queryset()
+        equipments = self.filter_queryset(queryset)
+        page_list = self.paginate_queryset(equipments)
+
+        serializer = self.get_serializer(page_list, many=True)
+
+        response_dict['code'] = 200
+        response_dict['message'] = 'Success'
+        data_dict = {'list': serializer.data, 'pagination': {}}
+        data_dict['pagination']['current_page'] = self.paginator.page.number
+        data_dict['pagination']['num_pages'] = self.paginator.page.paginator.num_pages
+        data_dict['pagination']['per_page'] = self.paginator.page.paginator.per_page
+        data_dict['pagination']['total_size'] = len(equipments)
+        response_dict['data'] = data_dict
+
         return Response(data=response_dict, status=status.HTTP_200_OK)
 
     @ action(methods=['put'], detail=False, url_path='update')
