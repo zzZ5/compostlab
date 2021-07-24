9b6d152c2597410210fdf006c14ff77535ba1e2d
zzZ5 <baoju_liu@foxmail.com>
2021-07-24 16:19:06 +0800

    mqtt

--- compostlab/wsgi.py
+++ compostlab/wsgi.py
@@ -8,10 +8,11 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-from compostlab.utils.mqtt import Mqtt
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'compostlab.settings')
 
 application = get_wsgi_application()
+
+from compostlab.utils.mqtt import Mqtt
 mqtt = Mqtt()
